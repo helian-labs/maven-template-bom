37a44839bfddfd82da7a7462ca0684a7c16acf5d
zhycn <qizhaohong_sh@qq.com>
2025-04-12 22:11:13 +0800

    feat:add LICENSE

--- module2/src/test/java/com/helian/AppTest.java
+++ module2/src/test/java/com/helian/AppTest.java
@@ -4,15 +4,26 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * Unit test
+ * Unit test for App class
  */
 public class AppTest {
 
     /**
-     * What is this.
+     * 测试App类的main方法能否正常执行
      */
     @Test
-    public void notARealTest() {
+    public void testAppMain() {
+        // 验证main方法能否正常执行而不抛出异常
+        App.main(new String[]{});
         Assert.assertTrue(true);
+    }
+    
+    /**
+     * 测试App是否能正确使用Speaker
+     */
+    @Test
+    public void testAppUsesSpeaker() {
+        Speaker speaker = new Speaker();
+        Assert.assertNotNull("Speaker should return a message", speaker.speak());
     }
 }
